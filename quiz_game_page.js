96b44a9e8afbaf34c0664b1746124a7a6ef905a4
Abhiveer773 <90145630+Abhiveer773@users.noreply.github.com>
2022-03-19 11:28:21 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -15,3 +15,26 @@ document.getElementById("output").innerHTML = row ;
 
 document.getElementById("number1").value = "";
 document.getElementById("number2").value = "";
+
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check()
+{
+    get_answer = document.getElementById("input_check_box").value;
+
+    if(get_answer == actual_answer)
+    {
+        if(answer_turn == "player1")
+        {
+            update_player1_score = player1_score+1;
+            document.getElementById("player1_score").innerHTML = update_player1_score;
+        }
+        else
+        {
+            update_player2_score = player2_score+1;
+            document.getElementById("player_quetion").innerHTML = "Question Turn -" + player1_name;
+        }
+    }
+}
